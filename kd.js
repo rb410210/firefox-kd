27dcaed251415cf15312b00a7b6a45ff068f705e
Rohit <rohitbalan82@gmail.com>
2018-03-09 01:44:59 -0500

    loader

--- kd.js
+++ kd.js
@@ -1,1 +1,43 @@
+browser.contextMenus.create({
+  id: "pushToKodiLink",
+  title: "Push to Kodi",
+  contexts: ["link"]
+});
 
+browser.contextMenus.create({
+  id: "pushToKodiVideo",
+  title: "Push to Kodi",
+  contexts: ["video"]
+});
+
+function onResponse(response) {
+  console.log("Received " + response);
+  browser.tabs.executeScript({
+    file: "passive.js"
+  });
+
+}
+
+function onError(error) {
+  console.log(`Error: `, error);
+  browser.tabs.executeScript({
+    file: "passive.js"
+  });
+}
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info && info.menuItemId === 'pushToKodiLink') {
+    console.log("Push website to kodi", info.linkUrl);
+
+    browser.tabs.executeScript({
+      file: "active.js"
+    });
+
+    var sending = browser.runtime.sendNativeMessage("firefox_kd_native",
+      info.linkUrl);
+    sending.then(onResponse, onError);
+  }
+  if (info && info.menuItemId === 'pushToKodiVideo') {
+    console.log("Push video to kodi", info, tab);
+  }
+});
